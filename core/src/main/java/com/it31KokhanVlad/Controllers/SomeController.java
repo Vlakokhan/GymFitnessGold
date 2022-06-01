5c9d99d31ef92e4b71fcb0a5eff4ae2659c1967f
Kokhan_pro <vladislav27ko@gmail.com>
2022-06-01 20:40:12 +0300

    first plan

---
+++ core/src/main/java/com/it31KokhanVlad/Controllers/SomeController.java
@@ -0,0 +1,15 @@
+package com.it31KokhanVlad.Controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SomeController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+    return "Hello";
+    }
+}
---
+++ core/src/test/java/com/it31KokhanVlad/GoldGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it31KokhanVlad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
